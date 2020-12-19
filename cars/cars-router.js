7eff7e0a586cd857de979e7b0edbb2f5ad632d7f
Heather Beckman <heatherbeckman85@gmail.com>
2020-12-19 10:06:27 -0700

    created new car, tested with insomnia

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -22,4 +22,15 @@ router.get("/cars/:id", async (req, res, next) => {
     }
 })
 
+router.post("/cars", async (req, res, next) => {
+    try{
+        const [id] = await db("cars").insert(req.body)
+        const newCar = await db("cars").where({id}).first()
+        
+        res.status(201).json(newCar)
+    } catch(err) {
+        next(err)
+    }
+})
+
 module.exports = router
